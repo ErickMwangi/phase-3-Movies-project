6648ac3d92de90077e62ea0a1a964b567497e1b1
ErickMwangi <rickthetri20@gmail.com>
2023-06-07 17:51:41 +0300

    initial commit

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine("sqlite:///movies.db")
@@ -13,4 +13,18 @@ class Movie(Base):
     title = Column(String(), index=True)
     release_year = Column(Integer(), index=True)
 
-    
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer(), primary_key=True)
+    username = Column(String)
+    age = Column(Integer, index=True)
+
+class Genre(Base):
+    __tablename__ = "genres"
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(), index=True)
+
+class WatchedMovie(Base):
+    __tablename__ = "watched_movies"
+    user_id = Column(Integer(), ForeignKey("users_id"))
+    movie_id = Column(Integer(),ForeignKey("movie_id"))
